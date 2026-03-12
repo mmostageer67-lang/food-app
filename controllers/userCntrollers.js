aaf9f96252df8920c60bd841cb08ae579a3fc234
mahmoud mostageer <mmostageer67@gmail.com>
2026-03-12 21:22:03 +0200

    get user

--- controllers/userCntrollers.js
+++ controllers/userCntrollers.js
@@ -1,7 +1,34 @@
+const userModel=require('../models/userModel')
 const getUserControllers=async(req,res)=>{
-    res.status(200).json({
-        message:'get all data'
+    try
+    {
+const user=await userModel.findById({_id:req.body.id},{_id:0})
+if(!user)
+{
+    res.status(404).json({
+        succes:false,
+        message:'error in id '
     })
 }
+user.password=undefined
+res.status(201).json({
+    succes:true,
+    message:'get the user',
+    user 
+})
+    }catch(error)
+    {
+        res.status(500).json({
 
-module.exports={getUserControllers}
+            succes:false,
+            message:'we have problem in api',
+            error
+        })
+    }
+}
+
+const updateUser=()=>
+{
+
+}
+module.exports={getUserControllers,updateUser}
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,6 +1,7 @@
 const express=require('express')
-const { getUserControllers } = require('../controllers/userCntrollers')
+const { getUserControllers, updateUser } = require('../controllers/userCntrollers')
 const authMiddleware = require('../middleware/authMiddleware')
 const router =express.Router()
 router.get('/getUser',authMiddleware,getUserControllers)
+router.put('/updateUser',authMiddleware,updateUser)
 module.exports=router
