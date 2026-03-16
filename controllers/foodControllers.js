da2187f8fb29d210efeea13ab5ff98f95416f798
mahmoud mostageer <mmostageer67@gmail.com>
2026-03-17 01:47:02 +0200

    the order

--- controllers/foodControllers.js
+++ controllers/foodControllers.js
@@ -177,5 +177,39 @@ const placeOrderController = async (req, res) => {
     });
   }
 };
+// CHANGE ORDER STATUS
 
-module.exports={creatFood,updateFood,deleteFood,placeOrderController}
+const orderStatusController = async (req, res) => {
+  try {
+    const orderId = req.params.id;
+
+    if (!orderId) {
+      return res.status(404).send({
+        success: false,
+        message: "Please Provide valid order id",
+      });
+    }
+
+    const { status } = req.body;
+
+    const order = await orderModel.findByIdAndUpdate(
+      orderId,
+      { status },
+      { new: true }
+    );
+
+    res.status(200).send({
+      success: true,
+      message: "Order Status Updated",
+    });
+
+  } catch (error) {
+    console.log(error);
+    res.status(500).send({
+      success: false,
+      message: "Error in Update Order API",
+      error,
+    });
+  }
+};
+module.exports={creatFood,updateFood,deleteFood,placeOrderController,orderStatusController}
--- routes/foodRoutes.js
+++ routes/foodRoutes.js
@@ -1,12 +1,13 @@
 const express=require('express')
 const authMiddleware = require('../middleware/authMiddleware')
-const { creatFood, updateFood, deleteFood, placeOrderControllers, placeOrderController } = require('../controllers/foodControllers')
+const { creatFood, updateFood, deleteFood, placeOrderControllers, placeOrderController, postOrder, orderStatusController } = require('../controllers/foodControllers')
 const router=express.Router()
 
 router.post('/create',authMiddleware,creatFood)
 router.put('/update/:id',authMiddleware,updateFood)
 router.delete('/delete/:id',authMiddleware,deleteFood)
 router.post('/placeorder',authMiddleware,placeOrderController)
+router.post('/orderStatus/:id',authMiddleware,authMiddleware,orderStatusController)
 
 module.exports=router
 
