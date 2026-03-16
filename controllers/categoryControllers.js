42ef3566d1ff8b36a2b28989755cb2288a9410a4
mahmoud mostageer <mmostageer67@gmail.com>
2026-03-16 23:42:35 +0200

    get all and update category

--- controllers/categoryControllers.js
+++ controllers/categoryControllers.js
@@ -26,4 +26,62 @@ res.status(201).json({
         })
     }
 }
-module.exports = creatCategoryController
+const getAllCategorys=async(req,res)=>
+{
+    try {
+        const categorys=await categoryModel.find({} )
+        if(!categorys)
+        {
+            res.status(500).json({
+                cuccess:false,
+                message:'CATEGORY NOT FOUND'
+            })
+        }
+        res.status(200).json({
+            cuccess:true,
+            message:'GET ALL CATEGORYS',
+totalCat:categorys.length,
+            categorys
+        })
+    } catch (error) {
+        res.status(500).json({
+            succes:true,
+            message:'error in get categories api.',
+            error
+        })
+    }
+}
+const updateCategory=async(req,res)=>
+{
+    try {
+        const {id}=req.params
+        const{title,imageUrl}=req.body
+        if(!title||imageUrl)
+        {
+            res.status(500).json({
+                succes:false,
+                message:'PLEASE PROVIDE TITLE OR IMAGEURL'
+            })
+        }
+       const updateCatego = await categoryModel .findByIdAndUpdate(id,{title,imageUrl},{new:true})
+         if(!updateCatego)
+         {
+           res.status(500).json({
+                succes:false,
+                message:'update category not found!'
+            }) 
+         }
+         res.status(200).json({
+            cuccess:true,
+            message:'updated successfully.',
+            updateCatego
+         })
+    } catch (error) {
+        res.status(500).json({
+            cuccess:false,
+            message:'error in update category!',
+            error
+        })
+    }
+}
+module.exports = {creatCategoryController,getAllCategorys,updateCategory}
--- routes/categoryRoutes.js
+++ routes/categoryRoutes.js
@@ -1,7 +1,11 @@
 const express=require('express')
 const authMiddleware = require('../middleware/authMiddleware')
-const creatCategoryController = require('../controllers/categoryControllers')
+const {creatCategoryController, getAllCategorys, updateCategory} = require('../controllers/categoryControllers')
 const router=express.Router()
 router.post('/creat',authMiddleware,creatCategoryController)
+router.get('/getAll',getAllCategorys)
+router.put('/update/:id',authMiddleware,updateCategory)
+
 
 module.exports=router
+
