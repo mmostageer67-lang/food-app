7d7b6fb82da84bef062f2aa6b10d72900586cfa4
mahmoud mostageer <mmostageer67@gmail.com>
2026-03-17 01:36:20 +0200

    place order

--- controllers/foodControllers.js
+++ controllers/foodControllers.js
@@ -111,4 +111,71 @@ const deleteFood=async(req,res)=>
     })
   }
 }
-module.exports={creatFood,updateFood,deleteFood}
+const placeOrderControllers=async(req,res)=>
+{
+try {
+    const foodId=req.params.id
+    if(!foodId)
+    {return res.status.json({
+      success:false,
+      message:'THE ID NOT FOUND || RESTURANT !'
+    })}
+    await foodModel.findByIdAndDelete(foodId)
+
+    
+
+    res.status(200).json({
+      success:true,
+      message:'food DELETED SUCCESSFULLY'
+    })
+    
+
+  } catch (error) {
+    res.status(500).json({
+      success:false,
+      message:'ERROR IN DELETE API!',
+      error
+    })
+  }
+}
+// PLACE ORDER
+const placeOrderController = async (req, res) => {
+  try {
+    const { cart, payment } = req.body;
+
+    if (!cart || !payment) {
+      return res.status(500).send({
+        success: false,
+        message: "please food cart or payment method",
+      });
+    }
+
+    const total = 0;
+
+    // cal
+    cart.map((i) => {
+      total += i.price;
+    });
+
+    const newOrder = new orderModel({
+      foods: cart,
+      payment,
+      buyer: req.body.id,
+    });
+
+    res.status(201).send({
+      success: true,
+      message: "Order Placed successfully",
+      newOrder,
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(500).send({
+      success: false,
+      message: "Error in place order API",
+      error,
+    });
+  }
+};
+
+module.exports={creatFood,updateFood,deleteFood,placeOrderController}
--- routes/foodRoutes.js
+++ routes/foodRoutes.js
@@ -1,11 +1,12 @@
 const express=require('express')
 const authMiddleware = require('../middleware/authMiddleware')
-const { creatFood, updateFood, deleteFood } = require('../controllers/foodControllers')
+const { creatFood, updateFood, deleteFood, placeOrderControllers, placeOrderController } = require('../controllers/foodControllers')
 const router=express.Router()
 
 router.post('/create',authMiddleware,creatFood)
 router.put('/update/:id',authMiddleware,updateFood)
 router.delete('/delete/:id',authMiddleware,deleteFood)
+router.post('/placeorder',authMiddleware,placeOrderController)
 
 module.exports=router
 
